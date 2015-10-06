7b8abf2842e40585aa6b849c657f5cf919503994
skazantsev <sergey.kazantsev1@gmail.com>
2015-10-06 23:29:48 +0300

    Published a package to NuGet

--- src/WebDav/WebDav.Client/Properties/AssemblyInfo.cs
+++ src/WebDav/WebDav.Client/Properties/AssemblyInfo.cs
@@ -1,10 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("WebDav Client")]
-[assembly: AssemblyProduct("WebDav Client")]
+[assembly: AssemblyTitle("WebDav.Client")]
+[assembly: AssemblyCompany("skazantsev")]
+[assembly: AssemblyProduct("WebDav.Client")]
+[assembly: AssemblyDescription("An easy-to-use async WebDAV client for .NET")]
 [assembly: AssemblyCopyright("Copyright © 2015 skazantsev")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0cc4b1b7-57f0-456a-8512-b8d69e34a550")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
