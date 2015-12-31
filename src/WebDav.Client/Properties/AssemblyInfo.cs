649e421e99613d53a28a3be92ba9b609bfdc6cf1
skazantsev <sergey.kazantsev1@gmail.com>
2015-12-31 06:14:55 +0300

    v1.0.1 in nuget

--- src/WebDav.Client/Properties/AssemblyInfo.cs
+++ src/WebDav.Client/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2015 skazantsev")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0cc4b1b7-57f0-456a-8512-b8d69e34a550")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 [assembly: InternalsVisibleTo("WebDav.Client.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
