f4a8bf4b5f372ce6e7b883a6ac0582142272e93c
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2017-08-20 14:49:19 +0200

    #7: support .NET Core

--- build/build.ps1
+++ build/build.ps1
@@ -1,67 +1,21 @@
 properties {
-    $version = "1.1.0.0"
-    $signAssemblies = $false
-    $signKeyPath = "C:\snkey\skazantsev.snk"
-
     $baseDir = resolve-path ..
     $srcDir = "$baseDir\src"
-    $slnFile = "$srcDir\WebDav.sln"
+    $slnPath = "$srcDir\WebDav.sln"
     $releaseDir = "$baseDir\release"
-    $nugetPath = "$srcDir\.nuget\nuget.exe"
-    $xunitPath = "$srcDir\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe"
+    $global:config = "release"
 }
 
-task default -depends Build, RunTests, CreateNuGetPackage
+task default -depends build, test, pack
 
-task Build -depends RestorePackages {
-    Update-AssemblyInfoVersion $srcDir $version
-
-    $constants = "TRACE"
-    if ($signAssemblies -eq $true) {
-        $constants += ";SIGNED"
-    }
-    exec { msbuild /nologo $slnFile "/t:Clean;Rebuild" /p:Configuration=Release /p:TargetFrameworkVersion=v4.5 /p:DefineConstants=`"$constants`" "/p:SignAssembly=$signAssemblies" "/p:AssemblyOriginatorKeyFile=$signKeyPath"  }
+task build {
+    exec { dotnet build $slnPath -c $config  /v:m /nologo }
 }
 
-task RestorePackages {
-    exec { . $nugetPath restore $slnFile }
+task test -depends Build {
+    exec { dotnet test $srcDir\WebDav.Client.Tests\WebDav.Client.Tests.csproj -c $config /nologo }
 }
 
-task RunTests -depends Build {
-    exec {. $xunitPath "$srcDir\WebDav.Client.Tests\bin\Release\WebDav.Client.Tests.dll" }
-}
-
-task CreateNuGetPackage -depends Build, RunTests {
-    mkdir "$releaseDir\lib\net45" -force | Out-Null
-    rm "$releaseDir\lib\net45\*"
-    cp "$srcDir\WebDav.Client\bin\Release\WebDav.Client.dll" "$releaseDir\lib\net45"
-    cp "$baseDir\build\WebDav.Client.nuspec" "$releaseDir"
-
-    $nuspec = [xml](cat "$releaseDir\WebDav.Client.nuspec" -encoding UTF8)
-    $nuspec.package.metadata.version = $version
-
-    write-host "Creating a nuget package with nuspec file:"
-    write-host $nuspec.OuterXml
-
-    $nuspec.Save("$releaseDir\WebDav.Client.nuspec")
-
-    exec { . $nugetPath pack "$releaseDir\WebDav.Client.nuspec" -BasePath "$releaseDir" -OutputDirectory "$releaseDir" }
-
-    if ($signAssemblies -eq $true) {
-        write-host "Created signed package!" -foreground green
-    } else {
-        write-host "[WARN] Created package is not signed!" -foreground yellow
-    } 
-}
-
-function Update-AssemblyInfoVersion([string] $srcDir, [string] $version) {
-    get-childitem -path $srcDir -recurse -filter AssemblyInfo.cs | foreach {
-        $filename = $_.FullName
-        write-host "Setting $version version for $filename"
-
-        (cat $filename -encoding UTF8) `
-            -replace "AssemblyVersion\(""[0-9]+(\.([0-9]+|\*)){1,3}""\)", "AssemblyVersion(""$version"")" `
-            -replace "AssemblyFileVersion\(""[0-9]+(\.([0-9]+|\*)){1,3}""\)", "AssemblyFileVersion(""$version"")" `
-        | out-file $filename -encoding UTF8
-    }
+task pack {
+    exec { dotnet pack $srcDir\WebDav.Client\WebDav.Client.csproj -c $config -o $releaseDir --no-build }
 }
--- src/WebDav.Client/Properties/AssemblyInfo.cs
+++ src/WebDav.Client/Properties/AssemblyInfo.cs
@@ -1,26 +1,18 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+
+#if NET45
 using System.Security;
-
-[assembly: AssemblyTitle("WebDav.Client")]
-[assembly: AssemblyCompany("skazantsev")]
-[assembly: AssemblyProduct("WebDav.Client")]
-[assembly: AssemblyDescription("An easy-to-use async WebDAV client for .NET")]
-[assembly: AssemblyCopyright("Copyright © 2017 skazantsev")]
-[assembly: ComVisible(false)]
-[assembly: Guid("0cc4b1b7-57f0-456a-8512-b8d69e34a550")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-
-#if SIGNED
-[assembly: InternalsVisibleTo("WebDav.Client.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f130ca0ab17e20a726c71d1629f62002952816a0e1c5dc255118b348a8137f0e9c4b96f448d680a6fc72b30b1c28b7d8b19dca5c812f76230c51c8247e3ee687725635ec749a1f32bcf540913d9bd04d40cd834e9c1103cc4ec47669a0f6348a35fd11d1f935a39d8bfe9721e66f9996ccfa7e88cf38e1806e9cbc1b7256c9b6")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
-#else
-[assembly: InternalsVisibleTo("WebDav.Client.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 #endif
 
 [assembly: CLSCompliant(true)]
+
+#if NET45
 [assembly: AllowPartiallyTrustedCallers]
+#endif
+
+[assembly: InternalsVisibleTo("WebDav.Client.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f130ca0ab17e20a726c71d1629f62002952816a0e1c5dc255118b348a8137f0e9c4b96f448d680a6fc72b30b1c28b7d8b19dca5c812f76230c51c8247e3ee687725635ec749a1f32bcf540913d9bd04d40cd834e9c1103cc4ec47669a0f6348a35fd11d1f935a39d8bfe9721e66f9996ccfa7e88cf38e1806e9cbc1b7256c9b6")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
+
+//[assembly: InternalsVisibleTo("WebDav.Client.Tests")]
+//[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
