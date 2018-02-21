d6a2463c73420a0840674c283c78e1419e21c3a6
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2018-02-21 23:56:55 +0100

    #14: support custom headers in individual calls

---
+++ src/WebDav.Client/Domain/WebDavHeaders.cs
@@ -0,0 +1,19 @@
+﻿namespace WebDav.Client.Domain
+{
+    internal static class WebDavHeaders
+    {
+        public static readonly string Depth = "Depth";
+
+        public static readonly string Destination = "Destination";
+
+        public static readonly string If = "If";
+
+        public static readonly string LockToken = "Lock-Token";
+
+        public static readonly string Overwrite = "Overwrite";
+
+        public static readonly string Timeout = "Timeout";
+
+        public static readonly string Translate = "Translate";
+    }
+}
