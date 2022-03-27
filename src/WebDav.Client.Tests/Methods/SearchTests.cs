1ca349f1c5fa20af997781204adef46213846a3c
Luc Pasmans <mail@lucpasmans.nl>
2022-03-27 13:51:20 +0200

    Improved search parameters handling, two basic tests, fix iwebdavclient typo.

---
+++ src/WebDav.Client.Tests/Methods/SearchTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using WebDav.Client.Tests.TestDoubles;
+using Xunit;
+
+namespace WebDav.Client.Tests.Methods
+{
+    public class SearchTests
+    {
+        [Fact]
+        public async void When_RequestIsSuccessfull_Should_ReturnStatusCode200()
+        {
+            var client = new WebDavClient().SetWebDavDispatcher(Dispatcher.Mock());
+            var response1 = await client.Search(new Uri("http://example.com/"), new Request.SearchParameters()
+            {
+                SearchPath = "/root/",
+                SearchKeyword = "test%",
+                SelectProperties = new[]
+                           {
+                                new XElement("{DAV:}displayname"),
+                                new XElement("{DAV:}getcontenttype")
+                           },
+                WhereProperties = new[]
+                           {
+                                new XElement("{DAV:}displayname")
+                           }
+            });
+
+            Assert.Equal(200, response1.StatusCode);
+        }
+
+        [Fact]
+        public async void When_RequestIsBadRequest_Should_ReturnStatusCode400()
+        {
+            var client = new WebDavClient().SetWebDavDispatcher(Dispatcher.Mock());
+            var response1 = await client.Search(new Uri("http://example.com/"), new Request.SearchParameters());
+            Assert.Equal(400, response1.StatusCode);
+        }
+
+    }
+}
--- src/WebDav.Client/Request/SearchParameters.cs
+++ src/WebDav.Client/Request/SearchParameters.cs
@@ -8,32 +8,48 @@ namespace WebDav.Client.Request
 {
     public class SearchParameters
     {
+        public SearchParameters()
+        {
+            Namespaces = new List<NamespaceAttr>();
+            Headers = new List<KeyValuePair<string, string>>();
+            SelectProperties = new List<XElement>();
+            WhereProperties = new List<XElement>();
+            CancellationToken = CancellationToken.None;
+        }
+
         /// <summary>
         /// Root directory where to search from.
         /// </summary>
         public string SearchPath { get; set; }
+       
         /// <summary>
         /// Keyword on what to search. For example: 'test%' (without quotes), results in LIKE test%.
         /// </summary>
         public string SearchKeyword { get; set; }
+       
+        /// <summary>
+        /// Set the cancellation token.
+        /// </summary>
         public CancellationToken CancellationToken { get; set; }
 
         /// <summary>
         /// Gets or sets the collection of xml namespaces of properties.
         /// </summary>
         public IReadOnlyCollection<NamespaceAttr> Namespaces { get; set; }
+
         /// <summary>
         /// Gets or sets the collection of the headers.
         /// </summary>
         public IReadOnlyCollection<KeyValuePair<string, string>> Headers { get; set; }
+
         /// <summary>
         /// Gets or sets the Select properties.
         /// </summary>
         public IReadOnlyCollection<XElement> SelectProperties { get; set; }
+
         /// <summary>
         /// Gets or sets the Where properties.
         /// </summary>
         public IReadOnlyCollection<XElement> WhereProperties { get; set; }
-
     }
 }
